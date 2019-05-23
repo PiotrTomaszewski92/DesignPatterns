90498d1b65eb2ad03903b0088bbb8f73b21a9537
PiotrTomaszewski <piotrekha15@gmail.com>
2019-05-23 15:45:24 +0200

    prepare class to create a singleton pattern

---
+++ src/creational/singleton/GameEngine.java
@@ -0,0 +1,15 @@
+package creational.singleton;
+
+public class GameEngine {
+
+    private int hp=100;
+    private String characterName = "";
+
+    public void run(){
+        while(true){
+            //czekamy na input od gracza
+            //zmieniamy stan gry
+            //renderujemy grafikę
+        }
+    }
+}
