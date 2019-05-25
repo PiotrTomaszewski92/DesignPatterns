932dab9e1d0a2ed558a69b06a75eee2ec3b869ff
PiotrTomaszewski <piotrekha15@gmail.com>
2019-05-25 17:00:37 +0200

    imlpemented flyweigh design pattern with example

--- src/structural/flyweight/Main.java
+++ src/structural/flyweight/Main.java
@@ -2,7 +2,7 @@ package structural.flyweight;
 
 import structural.flyweight.units.Destroyer;
 import structural.flyweight.units.Rifleman;
-import structural.flyweight.units.TeslTank;
+import structural.flyweight.units.TeslaTank;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,10 +11,10 @@ public class Main{
     public static void main(String[] args) {
         List<Object> activeUnits = new ArrayList<>();
 
-        for(int i = 9; i<1000; i++){
-            activeUnits.add(new TeslTank("Tesla Tank",200,100,50,25,0,0,500));
-            activeUnits.add(new Rifleman("Rifleman",25,5,20,25,0,0,50));
-            activeUnits.add(new Destroyer("Destroyer",500,250,70,10,0,0,800));
+        for(int i = 1; i<1000; i++){
+            activeUnits.add(new TeslaTank(0,0));
+            activeUnits.add(new Rifleman(0,0));
+            activeUnits.add(new Destroyer(0,0));
         }
     }
 }
---
+++ src/structural/flyweight/units/UnitStatsRepository.java
@@ -0,0 +1,25 @@
+package structural.flyweight.units;
+
+public class UnitStatsRepository {
+
+    private static UnitStats destroyerStats
+            = new UnitStats("Destroyer",500,250,70,10,800);
+    private static UnitStats teslaTankStats
+            = new UnitStats("Tesla Tank",200,100,50,25,500);
+    private static UnitStats riflemanStats
+            = new UnitStats("Rifleman",25,5,20,25,50);
+
+    public static UnitStats getDestroyerStats(){
+        return destroyerStats;
+    }
+
+    public static UnitStats getTeslaTankStats(){
+        return teslaTankStats;
+    }
+
+    public static UnitStats getRiflemanStats(){
+        return riflemanStats;
+    }
+}
+
+
