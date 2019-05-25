240b13b46e4cac5fcffd9ae0d977a3a55e8f3999
PiotrTomaszewski <piotrekha15@gmail.com>
2019-05-25 18:52:38 +0200

    implemented 2way adapter

--- src/structural/adapter/Main.java
+++ src/structural/adapter/Main.java
@@ -2,26 +2,25 @@ package structural.adapter;
 
 public class Main {
     public static void main(String[] args) {
-        ContinentalDevice radio = new ContinentalDevice() {
+        ContinentalDevice continentalRadio = new ContinentalDevice() {
             @Override
             public void on() {
                 System.out.println("Music from radio: on.");
             }
         };
-        ContinentalSocket continentalSocket = new ContinentalSocket();
-        continentalSocket.plugIn(radio);
-        //=========================
         UKDevice englishRadio = new UKDevice() {
             @Override
             public void powerOn() {
                 System.out.println("Music from English radio: on");
             }
         };
+        ContinentalSocket continentalSocket = new ContinentalSocket();
         UKSocket ukSocket = new UKSocket();
-        ukSocket.plugIn(englishRadio);
 
-        UKToContinentalAdapter adapter = new UKToContinentalAdapter(englishRadio);
-        continentalSocket.plugIn(adapter);
+        TwoWayAdapter adapter = new TwoWayAdapter(englishRadio,continentalRadio);
+        continentalSocket.plugIn(adapter); //odpalamy radio angielskie
+        ukSocket.plugIn(adapter); //odpalamy polskie radio
+
     }
 }
 
